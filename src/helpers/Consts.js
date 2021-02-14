3e46263c79768a455bc49bb0be783bc03626eea9
Gilliard Lopes <gilalan@gmail.com>
2021-02-13 23:21:04 -0300

    ajustando urls da build

--- src/helpers/Consts.js
+++ src/helpers/Consts.js
@@ -1,5 +1,6 @@
 //Constantes para APIs
-export const API_BASE_URL = 'http://localhost:3003/api';
+//export const API_BASE_URL = 'http://localhost:3003/api';
+export const API_BASE_URL = 'https://billingcycle-frontend.herokuapp.com/api';
 export const TAB_SELECTED = 'TAB_SELECTED';
 export const TAB_SHOWED = 'TAB_SHOWED';
 export const BILLING_SUMMARY_FETCHED = 'BILLING_SUMMARY_FETCHED';
